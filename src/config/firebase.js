d9d0656b25f616217d5de8ffb2e6dbfa836cde76
prviegas <prvieg@gmail.com>
2025-06-27 13:32:07 -0300

    new hosting

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,14 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 // Firebase configuration
-// You'll need to replace these with your actual Firebase project credentials
 const firebaseConfig = {
-  apiKey: "AIzaSyAhhZhSl3_miWy9erpNc_Hg-tseOCGBAqQ",
-  authDomain: "companion-afef7.firebaseapp.com",
-  projectId: "companion-afef7",
-  storageBucket: "companion-afef7.firebasestorage.app",
-  messagingSenderId: "151052836594",
-  appId: "1:151052836594:web:8520fb54af4a95dab63a6b",
-  measurementId: "G-CK6DCPY0ND"
+  apiKey: "AIzaSyAVnqrOjr4L0zje00cHPdIo3eA78QQCNmc",
+  authDomain: "neurocompanion.firebaseapp.com",
+  projectId: "neurocompanion",
+  storageBucket: "neurocompanion.firebasestorage.app",
+  messagingSenderId: "936091023481",
+  appId: "1:936091023481:web:d9db2707de72bc29c7fca8",
+  measurementId: "G-VKB60JS1EK"
 };
 
 // Initialize Firebase
